75a24459ceaaec706e7934d92d2478c589b221b5
jrraymundo <jerome_raymundo@outlook.com>
2022-01-15 17:22:24 +0800

    Refactor fastet lap

--- src/challenges/FastestLap.java
+++ src/challenges/FastestLap.java
@@ -3,20 +3,31 @@ package challenges;
 import java.util.Scanner;
 
 public class FastestLap {
+    static double[] laps;
+    static double fastestLap = 0;
+    static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("How many laps? ");
-        double[] laps = new double[scanner.nextInt()];
+        getLapCount();
+        getLapTimes();
+        printLapDetails();
+    }
 
-        double fastestLap = 0;
+    public static void getLapCount() {
+        System.out.print("How many laps? ");
+        laps = new double[scanner.nextInt()];
+    }
+
+    public static void getLapTimes() {
         System.out.println("Input your lap times");
         for (int x = 0; x < laps.length; x++) {
             laps[x] = scanner.nextDouble();
             if (x == 0) fastestLap = laps[x];
             if (laps[x] < fastestLap) fastestLap = laps[x];
         }
+    }
 
+    public static void printLapDetails() {
         System.out.println("Lap Times");
         for (int x = 0; x < laps.length; x++) {
             System.out.println("Lap " + (x+1) + ": " + laps[x]);
