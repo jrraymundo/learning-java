869915e5a1ee09e942a35e565993fdd167efa690
jrraymundo <jerome_raymundo@outlook.com>
2023-07-23 15:59:12 +0800

    add comments in bounded type

--- src/fundamentals/BoundedTypeParameters.java
+++ src/fundamentals/BoundedTypeParameters.java
@@ -6,6 +6,10 @@ public class BoundedTypeParameters {
 		System.out.println(add(1, 2)); // returns 3.0
 	}
 	
+	// This is a good solution for creating a method
+	// that can add integer, float, double by using a bounded type parameter.
+	// Using Number as the bounded type is important to restrict
+	// the params to numbers only despite being a generic.
 	public static <T extends Number> double add(T num1, T num2) {
 		double result = num1.doubleValue() + num2.doubleValue();
 		return result;
